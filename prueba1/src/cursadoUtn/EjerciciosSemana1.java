621d3d634ae19c4c73e8261fbcd6bb374ef6e364
MatiasTimpanaro <matiasetimpanaro@hotmail.com>
2023-09-30 16:52:42 -0300

    Commit de la 2da semana

---
+++ prueba1/src/cursadoUtn/EjerciciosSemana1.java
@@ -0,0 +1,98 @@
+package cursadoUtn;
+
+public class EjerciciosSemana1 {
+ public static void main(String[] args) {
+/*
+ EJERCICIO 1 a
+ 
+ 	 int numeroInicio = 5;
+	 int numeroFin = 14;
+	 int contador = numeroInicio;
+	 
+	 while (contador < numeroFin) {
+		 
+		 System.out.print(contador + ", ");	
+		  contador++;
+		  }
+	 if (contador==numeroFin) { 		//if utilizado para que no aparezca la ultima ","
+		 System.out.print(contador);
+	 	}
+	 }
+  }
+ 
+ EJERCICIO 1 b
+ 
+	 int numeroInicio = 5;
+	 int numeroFin = 14;
+	 int contador = numeroInicio;
+	 
+	 while (contador < numeroFin) {
+		 if (contador%2 == 0) {
+			 System.out.print(contador + ", ");
+			 
+		 }
+		 contador++;
+		
+ 			}
+	 if (contador==numeroFin) { 		//if utilizado para que no aparezca la ultima ","
+		 System.out.print(contador);
+ 		}
+ 	}
+}
+
+	EJERCICIO 1 c 
+	 
+	 int numeroInicio = 5;
+	 int numeroFin = 14;
+	 int contador = numeroInicio;
+	 boolean esPar = true;
+	 
+	 while (contador <= numeroFin) {
+		 if ((contador%2 == 0) && (esPar==true)) {
+			 System.out.print(contador + " ");
+		 }
+		 
+			 if ((contador%2 != 0) && (esPar==false)) {
+				 System.out.print(contador + " ");
+		 }
+		 contador++;
+		 
+		 }
+	  }
+ }
+  
+  EJERCICIO 1 d
+
+	 int numeroInicio = 5;
+	 int numeroFin = 14;
+	 
+		for (int i = numeroFin; i>=numeroInicio; i--) {
+		 if (i%2 == 0) {
+			 
+		  System.out.print(i + " ");
+	 }
+	 } 
+ }
+}
+ 
+	
+	 EJERCICIO 2
+		  
+	 float ingresos = 0; //ingrese ingresos mensuales total del hogar
+	 int vehiculos = 0;  // ingrese la cantidad de vehiculos en posesion 
+	 int inmuebles = 0;  // ingrese la cantidad de inmuebles 
+	 int materialDeLujo = 0;  // ingrese si posee una embarcación, una aeronave de lujo o si es
+	 						 //titular de activos societarios que demuestren
+	 							//capacidad económica plena
+	 
+	 if ( (ingresos < 489083) && ( vehiculos < 3) && (inmuebles < 3) && (materialDeLujo < 1)) {
+		 System.out.println("Esta persona no pertenece al segmento de ingresos altos");
+	 } 
+	 else {
+		  System.out.println("Esta persona pertenece al segmento de ingresos altos");
+	 }
+ }
+}
+
+*/
+	 
