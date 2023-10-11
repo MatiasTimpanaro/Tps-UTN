788ed2e415d79be97fcd6eba91f05ec3a8af2949
MatiasTimpanaro <matiasetimpanaro@hotmail.com>
2023-10-11 18:49:22 -0300

    Se terminaron los ejercicios de la clase 6

---
+++ prueba1/src/cursadoUtn/Clase6Calculadora.java
@@ -0,0 +1,18 @@
+package cursadoUtn;
+
+public class Clase6Calculadora {
+	
+	public static double sumar (double n , double m) {
+		return (n + m);	
+	}
+	
+	public static double restar (double n , double m) {
+		return (n - m);	
+	}
+	public static double multiplicar (double n , double m) {
+		return (n * m);	
+	}
+	public static double dividir (double n , double m) {
+		return (n / m);	
+	}
+}
---
+++ prueba1/src/cursadoUtn/Clase6Testing.java
@@ -0,0 +1,26 @@
+package cursadoUtn;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Clase6Testing {
+
+	@Test
+	void multiplicarTest() {
+		assertEquals(Clase6Calculadora.multiplicar (80,3), 240);
+	}
+	@Test
+	void sumarTest() {
+		assertEquals(Clase6Calculadora.sumar (150,180)/3, 240);
+	}
+	@Test
+	void restarTest() {
+		assertNotEquals(Clase6Calculadora.restar (90,50)*15, 605);
+	}
+	@Test
+	void sumar2Test() {
+		assertNotEquals(Clase6Calculadora.restar (70,40)*25, 2700);
+	}
+
+}
